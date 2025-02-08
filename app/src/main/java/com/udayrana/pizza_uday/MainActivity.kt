9f4890961b5bd21bdea1e590b6cfd071deb60920
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2025-02-07 20:02:47 -0500

    Add top app bar, center layout, disable edge-to-edge

--- app/src/main/java/com/udayrana/pizza_uday/MainActivity.kt
+++ app/src/main/java/com/udayrana/pizza_uday/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.udayrana.pizza_uday
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.udayrana.pizza_uday.databinding.ActivityMainBinding
 
@@ -9,7 +8,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
